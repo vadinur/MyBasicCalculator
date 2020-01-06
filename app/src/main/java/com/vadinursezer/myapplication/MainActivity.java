963e80c1056900a1bd031a2f2bd5e80c605c9a47
vadinur <vadinur@gmail.com>
2020-01-06 22:20:22 +0300

    butterknife kütüphanesi eklendi.

--- app/src/main/java/com/vadinursezer/myapplication/MainActivity.java
+++ app/src/main/java/com/vadinursezer/myapplication/MainActivity.java
@@ -11,79 +11,49 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
-    // FIXME Butterknife ile okumak daha tatlı olur https://github.com/JakeWharton/butterknife
-    // FIXME @BindView(R.id.numberText1)
-    EditText numberText1;
-
-    // FIXME @BindView(R.id.numberText2)
-    EditText numberText2;
-
-    // FIXME @BindView(R.id.resultText)
-    TextView resultText;
-   
+    @BindView(R.id.numberText1) EditText numberText1;
+    @BindView(R.id.numberText2) EditText numberText2;
+    @BindView(R.id.resultText)  TextView resultText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
         setContentView(R.layout.activity_main);
-
-        // FIXME Butterknife olunca bu satıra gerek kalmayacak
-        numberText1 = findViewById(R.id.numberText1);
-        // FIXME Butterknife olunca bu satıra gerek kalmayacak
-        numberText2= findViewById(R.id.numberText2);
-        // FIXME Butterknife olunca bu satıra gerek kalmayacak
-        resultText=findViewById(R.id.resultText);
-        // FIXME Burada fazladan bir boşluk var :D ben azarlıyorum benim çömezler böyle fazla boşluk koyunca
-        // FIXME Burada fazladan bir boşluk var
-        // FIXME Burada fazladan bir boşluk var
-        // FIXME Burada fazladan bir boşluk var
+        ButterKnife.bind(this);
     }
-
-    // FIXME Tutarsızlık var bir üstteki function da ...State) { ama burda ...view){
-    // FIXME ) { veya ){ seç birini :)
     public void sum(View view){
 
         if(numberText1.getText().toString() .matches("") || numberText2.getText().toString().matches("")){
-            // FIXME r.strings 'den okumak tatlı olurdu
-            resultText.setText("Enter Number!");
-            // FIXME Burada fazladan bir boşluk var
-            // FIXME Burada fazladan bir boşluk var
+            resultText.setText(R.string.result);
         } else {
 
             int number1 = Integer.parseInt(numberText1.getText().toString());
             int number2 = Integer.parseInt(numberText2.getText().toString());
-
             int result = number1 + number2;
-            // FIXME Burada fazladan bir boşluk var
-            // FIXME String format çok hoş bişey :) https://stackoverflow.com/a/20887690
-            resultText.setText("Result: " + result);
 
+            resultText.setText(R.string.result);
             numberText1.setText("");
             numberText2.setText("");
         }
-        // FIXME Burada fazladan bir boşluk var
-        // FIXME Burada fazladan bir boşluk var
     }
 
     // FIXME ) { veya ){ seç birini :)
     public void deduct(View view){
 
         if(numberText1.getText().toString() .matches("") || numberText2.getText().toString().matches("")){
-            // FIXME r.strings 'den okumak tatlı olurdu
-            resultText.setText("Enter Number!");
-            // FIXME Burada fazladan bir boşluk var
-            // FIXME Burada fazladan bir boşluk var
+            resultText.setText(R.string.result);
         } else {
 
             int number1 = Integer.parseInt(numberText1.getText().toString());
             int number2 = Integer.parseInt(numberText2.getText().toString());
-
             int result = number1 - number2;
-            // FIXME String format çok hoş bişey :) https://stackoverflow.com/a/20887690
-            resultText.setText("Result: " + result);
 
+            resultText.setText(R.string.result);
             numberText1.setText("");
             numberText2.setText("");
         }
@@ -92,19 +62,14 @@ public class MainActivity extends AppCompatActivity {
     public void multiply(View view){
 
         if(numberText1.getText().toString() .matches("") || numberText2.getText().toString().matches("")){
-            // FIXME r.strings 'den okumak tatlı olurdu
-            resultText.setText("Enter Number!");
-            // FIXME Burada fazladan bir boşluk var
-            // FIXME Burada fazladan bir boşluk var
+            resultText.setText(R.string.result);
         } else {
 
             int number1 = Integer.parseInt(numberText1.getText().toString());
             int number2 = Integer.parseInt(numberText2.getText().toString());
-
             int result = number1 * number2;
-            // FIXME String format çok hoş bişey :) https://stackoverflow.com/a/20887690
-            resultText.setText("Result: " + result);
 
+            resultText.setText(R.string.result);
             numberText1.setText("");
             numberText2.setText("");
         }
@@ -113,25 +78,18 @@ public class MainActivity extends AppCompatActivity {
     public void divide(View view){
 
         if(numberText1.getText().toString() .matches("") || numberText2.getText().toString().matches("")){
-            // FIXME r.strings 'den okumak tatlı olurdu
-            resultText.setText("Enter Number!");
-            // FIXME Burada fazladan bir boşluk var
-            // FIXME Burada fazladan bir boşluk var
+            resultText.setText(R.string.result);
         } else {
 
             int number1 = Integer.parseInt(numberText1.getText().toString());
             int number2 = Integer.parseInt(numberText2.getText().toString());
-
             int result = number1 / number2;
-            // FIXME String format çok hoş bişey :) https://stackoverflow.com/a/20887690
-            resultText.setText("Result: " + result);
 
+            resultText.setText(R.string.result);
             numberText1.setText("");
             numberText2.setText("");
         }
     }
-    // FIXME Burada fazladan bir boşluk var
-    // FIXME Burada fazladan bir boşluk var
 }
 
 
